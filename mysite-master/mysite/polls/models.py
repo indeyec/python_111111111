e9325b97ec233c5a0c0ea7987c182c1450f43323
gr413_shivol <vlad.shilov03@mail.ru>
2022-12-13 16:27:04 +0700

    САМВЕЛ НЕ ЛЕЗЬ СЮДА!!!!

--- mysite-master/mysite/polls/models.py
+++ mysite-master/mysite/polls/models.py
@@ -32,7 +32,8 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
+    choice_text = models.CharField(max_length=200, validators=[
+        RegexValidator(), ])
     votes = models.IntegerField(default=0)
 
     def __str__(self):
